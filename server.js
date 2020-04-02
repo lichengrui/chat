1a093636b9e7ca40fd6eb14aa85f747c4dd5f4d8
vincentahn <jmanoba@hotmail.com>
2020-04-01 20:17:39 -0400

    Role Generator added and slight HTML format changes. Role generator added in script.js and server.js index.html format changes.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+// Command to start server: npm run devStart
+
 const io = require('socket.io')(3000)
 
 const users = {}
+
+function* roleDistributor(){
+  let roles = ['Liberal', 'Fascist', 'Hitler'];
+  var index;
+
+  while(roles.length > 0){
+    index = Math.floor(Math.random * roles.length);
+    yield roles.splice(index, 1);
+  }
+}
 
 io.on('connection', socket => {
     socket.on('new-user', name =>{
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
+        role = roleDistributor().next().value
+        socket.emit('role-given', {name: users[socket.id], role: role});
     })
     socket.on('send-chat-message', message=>{
         socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
