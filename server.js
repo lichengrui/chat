9fa79c75683a790e1ce84d65a78f5523f32856e8
vincentahn <jmanoba@hotmail.com>
2020-04-12 17:50:50 -0400

    Implemented Game Initialization and Role Distribution Included some additions to styles.css Included Start Game button in index.html Included sending 'start game' event to server and receiving 'test event' from server in script.js Included initialization of roles for start of game and edited role distribution in server.js. Also included server handling of 'start game' event.

--- server.js
+++ server.js
@@ -3,14 +3,30 @@
 const io = require('socket.io')(3000)
 
 const users = {}
+var roles = []
+
+function generateRoles(num_players){
+  switch(num_players){
+    case 3:
+      roles = ['Werewolf', 'Werewolf', 'Seer', 'Robber', 'Troublemaker', 'Villager'];
+      break;
+    case 4:
+      roles = ['Werewolf', 'Werewolf', 'Seer', 'Robber', 'Troublemaker', 'Villager', 'Villager'];
+      break;
+    case 5:
+      roles = ['Werewolf', 'Werewolf', 'Seer', 'Robber', 'Troublemaker', 'Villager', 'Villager', 'Villager'];
+      break;
+    default:
+      roles = ['Werewolf', 'Seer', 'Robber'];
+      break;
+  }
+}
 
 function* roleDistributor(){
-  let roles = ['Liberal', 'Fascist', 'Hitler'];
   var index;
 
   while(roles.length > 0){
     index = Math.floor(Math.random() * roles.length);
-    console.log(roles);
     yield roles.splice(index, 1);
   }
 }
@@ -19,11 +35,21 @@ io.on('connection', socket => {
     socket.on('new-user', name =>{
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
-        role = roleDistributor().next().value
-        socket.emit('role-given', {name: users[socket.id], role: role});
     })
     socket.on('send-chat-message', message=>{
         socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+    })
+    socket.on('start-game', () =>{
+        num_players = Object.getOwnPropertyNames(users).length
+        generateRoles(num_players)
+
+        console.log('Number of Players:', num_players)
+
+        for(player_idx in Object.getOwnPropertyNames(users)){
+          role = roleDistributor().next().value
+          user = Object.getOwnPropertyNames(users)[player_idx]
+          io.to(`${user}`).emit('role-given', role);
+        }
     })
     socket.on('disconnect', () =>{
         socket.broadcast.emit('user-disconnected', users[socket.id])
